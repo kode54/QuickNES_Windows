14af6e4b4578af26ec5934588ab7bf3464de2e42
Chris Moeller <kode54@gmail.com>
2012-03-09 16:22:41 -0800

    Fixed include paths

--- stdafx.h
+++ stdafx.h
@@ -34,7 +34,7 @@ extern CAppModule _Module;
   #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
-#include "../File_Extractor/fex/fex.h"
+#include <fex/fex.h>
 
 #include <nes_emu/Nes_Recorder.h>
 #include <nes_emu/Nes_Effects_Buffer.h>
